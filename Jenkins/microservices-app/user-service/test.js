03224625c044539c6d5063ee3d5634d2f2c266ba
harsh-dq <hasharma@deqode.com>
2021-08-02 14:26:49 +0530

    Jenkins Task

---
+++ Jenkins/microservices-app/user-service/test.js
@@ -0,0 +1,11 @@
+function emotify(string) {
+    return string + '' + ' :)'; 
+   }
+   
+   function anotherFunc(string) {
+    return string + '' + ' :)'; 
+   }
+
+   
+console.log(emotify('just testing User-servive'));
+console.log(anotherFunc('just testing Order-Service'));
