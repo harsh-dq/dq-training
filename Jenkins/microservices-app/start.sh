8106199124c0b49d4f8ced3d70aaad7584f9aec0
harsh-dq <hasharma@deqode.com>
2021-08-05 13:05:42 +0530

    Start Script for Microservice

--- Jenkins/microservices-app/start.sh
+++ Jenkins/microservices-app/start.sh
@@ -6,6 +6,7 @@ docker build -t ishu0824/userservice:$1 Jenkins/microservices-app/user-service/.
 docker build -t ishu0824/orderservice:$1 Jenkins/microservices-app/user-service/.
 
 
+docker rm -f routing userservice orderservice
 docker run -d -p 80:80  --net app --name routing ishu0824/routing:$1
 docker run -d -p 3000-3000  --net app --name userservice ishu0824/userservice:$1
 docker run -d -p 3001:3001  --net app --name orderservice ishu0824/orderservice:$1
